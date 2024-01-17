c29b35dca4a791de1098614d7aabd30222b31a16
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2024-01-17 15:48:11 +0530

    Update NestedQueries.sql

--- NestedQueries.sql
+++ NestedQueries.sql
@@ -37,3 +37,9 @@ INSERT INTO DEPARTMENT(Dname, Dnumber, MgrSSN, MgrStartDate) VALUES
 ('Accounting', 1, '45', '3/13/2023'),
 ('Services', 2, '97', '5/27/2023'),
 ('Support', 3, '69', '11/15/2023');
+INSERT INTO EMPLOYEE(Fname, Lname, SSN, Addrs, Sex, Salary, SuperSSN, Dno) VALUES
+('Luigi', 'Sutherby', '45', '68856 Lien Place', 'M', '261.42', '164', 1),
+('Curtice', 'Jorez', '97', '4629 Coolidge Road', 'M', '448.87', '032', 2),
+('Bridgette', 'Decruse', '69', '472 Bowman Drive', 'F', '751.51', '5', 3),
+('Feliza', 'Danielczyk', '29', '72238 Debs Lane', 'F', '929.90', '726', 4),
+('Archer', 'Morbey', '10', '9 Pond Circle', 'M', '774.36', '53', 5);
