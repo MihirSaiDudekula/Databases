3bd0f12a95d184da54cd075583d8645d4fe5deb0
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2024-01-24 15:07:43 +0530

    Create querySolving.sql

---
+++ querySolving.sql
@@ -0,0 +1,55 @@
+CREATE TABLE aircraft(
+  aid VARCHAR(9) PRIMARY KEY,
+  aname VARCHAR(10),
+  crange INT
+);
+ 
+CREATE TABLE employees(
+  eid VARCHAR(9) PRIMARY KEY,
+  ename VARCHAR(10),
+  salary INT
+);
+
+ CREATE TABLE certified(
+  aid VARCHAR(9),
+  eid VARCHAR(9),
+  FOREIGN KEY (eid) REFERENCES employees(eid),
+  FOREIGN KEY (aid) REFERENCES aircraft(aid)
+ );
+
+INSERT INTO aircraft VALUES
+('B001','Boeing',4000)
+('B002','Boeing',2500);
+('BB003','Blackbeard',6000);
+('S004','Supermarine',8000);
+('L005','Lockheed',2100);
+
+INSERT INTO employees VALUES
+(1,'Johnny',40000)
+(2,'Timmy',60000);
+(3,'Lawrence',70000);
+(4,'Zuzu',90000);
+(5,'Matt',80000);
+
+INSERT INTO certified VALUES
+('B001',1),
+('B002',1),
+('S004',3),
+('S004',4),
+('L005',5),
+('B002',2),
+('BB003',4),
+('BB003',3),
+('L005',4);
+
+-- Find Emp ID of employee who makes highest salary
+
+SELECT eid FROM employees WHERE salary=(SELECT MAX(salary) FROM employees);
+
+-- Find the name of aircrafts such that all pilots certified to operate them earn more than 50000
+
+SELECT aname FROM aircraft WHERE aid IN (SELECT aid FROM certified WHERE eid IN (SELECT eid FROM employees WHERE salary>50000));
+-- or
+SELECT	DISTINCT aname 
+FROM	aircraft a, certified c, employees e 
+WHERE	a.aid=c.aid and c.eid=e.eid and e.salary>50000;
