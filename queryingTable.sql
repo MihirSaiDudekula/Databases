72c11d4b3aa2bf67740f74a62543fd8a150c4a7d
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2024-02-20 19:24:59 +0530

    Update queryingTable.sql

--- queryingTable.sql
+++ queryingTable.sql
@@ -3,6 +3,15 @@
 SELECT sname FROM sailors WHERE sailid IN (SELECT sailid FROM reserves GROUP BY sailid HAVING count(sailid)>=1)
 -- or
 SELECT sname FROM Sailors S,Reserves R WHERE S.Sailid=R.Sailid GROUP BY R.sailid HAVING count(r.sailid)>=1;
+--or
+select sname
+from Sailors
+where exists(
+  select *
+  from Reserves 
+  where Sailors.sailid=Reserves.sailid
+ );
+
 
 -- 2. Find the Sid's of sailors who have reseved a red or a green boat
 SELECT sailid FROM reserves WHERE bid IN (SELECT bid FROM boats WHERE colour = 'red' OR colour = 'green');
